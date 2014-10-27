2e903309d885b5d31b18db101ea8ae346a4383e2
Jorge Martinez <jorgemg0327@gmail.com>
2014-10-27 17:08:21 -0500

    First commit

---
+++ STC_tracker.py
@@ -0,0 +1,51 @@
+import cv2
+import math
+import os
+import sys
+
+#def get_initbox(video_name)
+    
+
+
+if __name__ == '__main__':
+
+    dataset_folder = 'Datasets'
+    video_name = sys.argv[1]
+    frames_list = os.listdir(os.path.join(dataset_folder, video_name))
+    frames_list.sort()
+
+    # Get initial rectangle [x, y, width, height].
+    initstate = [161, 65, 75, 95]
+
+    # Center target.
+    pos = [initstate[1] + initstate[3]/2, initstate[0] + initstate[2]/2]
+    # Initial target size.
+    target_sz = [initstate[3], initstate[2]] 
+    
+    # Parameters according to the paper.
+    padding = 1                                 # Extra area.
+    rho = 0.075                                 # Learning parameter rho.
+    sz = math.floor(target_sz * (1. + padding))  # Context region size.
+
+    # Parameters of scale update - scale ratio, lambda, average frames.
+    scale, lambada, num = 1, 0.25, 5
+    # Pre-computed confidence map.
+    alapha = 2.25
+    #rs, cs.
+    #dist = rs^2 + cs^2
+    #conf = math.exp(-0.5 / (alapha) * math.sqrt(dist))
+
+    # Confidence map normalization
+    #conf =  
+     
+
+    # Loop reading frames
+    for frame in frames_list:
+        img = cv2.imread(os.path.join(dataset_folder, video_name, frame))
+        cv2.imshow('image', img)
+        key = cv2.waitKey(30)
+    
+        if key & 0xFF == ord('q'):
+            break
+
+    print "Loop finished!"
