2b3dfdb8504a29d2f285799e53a70715f97ca379
Jorge Martinez <jorgemg0327@gmail.com>
2014-10-28 17:41:34 -0500

    Code Matlab added

--- STC_tracker.py
+++ STC_tracker.py
@@ -1,17 +1,16 @@
 import cv2
 import math
+import numpy as np
 import os
 import sys
 
 #def get_initbox(video_name)
-    
-
 
 if __name__ == '__main__':
 
     dataset_folder = 'Datasets'
     video_name = sys.argv[1]
-    frames_list = os.listdir(os.path.join(dataset_folder, video_name))
+    frames_list = os.listdir(os.path.join(dataset_folder, video_name, 'img'))
     frames_list.sort()
 
     # Get initial rectangle [x, y, width, height].
@@ -20,12 +19,12 @@ if __name__ == '__main__':
     # Center target.
     pos = [initstate[1] + initstate[3]/2, initstate[0] + initstate[2]/2]
     # Initial target size.
-    target_sz = [initstate[3], initstate[2]] 
+    target_sz = np.array([initstate[3], initstate[2]])
     
     # Parameters according to the paper.
-    padding = 1                                 # Extra area.
+    padding = 1.0                               # Extra area.
     rho = 0.075                                 # Learning parameter rho.
-    sz = math.floor(target_sz * (1. + padding))  # Context region size.
+    sz = target_sz * (1. + padding)             # Context region size.
 
     # Parameters of scale update - scale ratio, lambda, average frames.
     scale, lambada, num = 1, 0.25, 5
@@ -33,15 +32,15 @@ if __name__ == '__main__':
     alapha = 2.25
     #rs, cs.
     #dist = rs^2 + cs^2
-    #conf = math.exp(-0.5 / (alapha) * math.sqrt(dist))
+    conf = math.exp(-0.5 / (alapha) * math.sqrt(642))
 
     # Confidence map normalization
     #conf =  
-     
 
     # Loop reading frames
     for frame in frames_list:
-        img = cv2.imread(os.path.join(dataset_folder, video_name, frame))
+        print os.path.join(dataset_folder, video_name, frame)       
+        img = cv2.imread(os.path.join(dataset_folder, video_name, 'img', frame))
         cv2.imshow('image', img)
         key = cv2.waitKey(30)
     
