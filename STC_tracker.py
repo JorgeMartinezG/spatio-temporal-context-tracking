d7977d380e2d01552568349acff2896789f75c8f
Jorge Martinez <jorgemg0327@gmail.com>
2014-10-29 16:57:02 -0500

    does not work

--- STC_tracker.py
+++ STC_tracker.py
@@ -3,6 +3,10 @@ import math
 import numpy as np
 import os
 import sys
+from scitools import numpyutils  as sn
+from scipy.io import loadmat
+
+mat = loadmat('stc_before_for.mat')
 
 #def get_initbox(video_name)
 
@@ -22,15 +26,22 @@ if __name__ == '__main__':
     target_sz = np.array([initstate[3], initstate[2]])
     
     # Parameters according to the paper.
-    padding = 1.0                               # Extra area.
+    padding = 1                               # Extra area.
     rho = 0.075                                 # Learning parameter rho.
-    sz = target_sz * (1. + padding)             # Context region size.
+    sz = target_sz * (1 + padding)             # Context region size.
 
     # Parameters of scale update - scale ratio, lambda, average frames.
     scale, lambada, num = 1, 0.25, 5
     # Pre-computed confidence map.
     alapha = 2.25
-    #rs, cs.
+
+    rs, cs = sn.ndgrid(np.array(range(sz[0])) +1 - np.floor(sz[0]/2), np.array(range(sz[1])) +1  - np.floor(sz[1]/2))
+
+    dist = rs**2 + cs**2
+
+    import ipdb;ipdb.set_trace()
+
+    #rs, cs = sn.ndgrid()
     #dist = rs^2 + cs^2
     conf = math.exp(-0.5 / (alapha) * math.sqrt(642))
 
@@ -39,7 +50,6 @@ if __name__ == '__main__':
 
     # Loop reading frames
     for frame in frames_list:
-        print os.path.join(dataset_folder, video_name, frame)       
         img = cv2.imread(os.path.join(dataset_folder, video_name, 'img', frame))
         cv2.imshow('image', img)
         key = cv2.waitKey(30)
